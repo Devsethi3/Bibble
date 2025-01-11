6ae0105e077eaaad849adc3cf70cc3686088b368
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-11 22:38:58 +0530

    Update DeleteServerModal.tsx

--- src/components/modals/DeleteServerModal.tsx
+++ src/components/modals/DeleteServerModal.tsx
@@ -36,7 +36,7 @@ const DeleteServerModal = () => {
       const response = await axios.delete(`/api/servers/${server.id}`);
 
       if (response.status === 200) {
-        // First
+        //
         onClose();
 
         // Show success message

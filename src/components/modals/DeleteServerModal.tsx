b693eb14efb3f479e8c146df6a5b612540506839
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-13 22:35:32 +0530

    Update DeleteServerModal.tsx

--- src/components/modals/DeleteServerModal.tsx
+++ src/components/modals/DeleteServerModal.tsx
@@ -43,7 +43,7 @@ const DeleteServerModal = () => {
         );
 
         try {
-          //
+          
           const { data: servers } = await axios.get("/api/servers");
 
           // Use setTimeout to ensure state updates have propagated

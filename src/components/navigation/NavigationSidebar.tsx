b58c808826fe8114828de4d53552ea7ed8a07907
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-18 21:39:51 +0530

    Update NavigationSidebar.tsx

--- src/components/navigation/NavigationSidebar.tsx
+++ src/components/navigation/NavigationSidebar.tsx
@@ -41,7 +41,7 @@ const NavigationSidebar = async () => {
         ))}
       </ScrollArea>
 
-      {/* Footer */}
+      {/*  */}
       <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
         {/* Theme Switcher */}
         <ThemeSwitcher />

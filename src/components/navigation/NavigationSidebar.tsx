3807d86f9db580428963fc84b6e8521e441fe253
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-19 22:05:10 +0530

    Update NavigationSidebar.tsx

--- src/components/navigation/NavigationSidebar.tsx
+++ src/components/navigation/NavigationSidebar.tsx
@@ -44,7 +44,6 @@ const NavigationSidebar = async () => {
       <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
         <ThemeSwitcher />
 
-        {/* */}
         <UserButton
           afterSignOutUrl="/"
           appearance={{

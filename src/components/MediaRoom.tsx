656ab2b98a138bb680b584840dadeb80f8e3b443
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2025-01-21 22:34:07 +0530

    Update MediaRoom.tsx

--- src/components/MediaRoom.tsx
+++ src/components/MediaRoom.tsx
@@ -21,7 +21,7 @@ const MediaRoom = ({ chatId, video, audio }: MediaRoomProps) => {
 
     const name = `${user.firstName} ${user?.lastName}`;
 
-    // Define and
+    // Define
     (async () => {
       try {
         const resp = await fetch(
